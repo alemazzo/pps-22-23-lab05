e6b6e376239202cae19614d0369edf836577e090
Alessandro Mazzoli <alessandro.mazzoli9@studio.unibo.it>
2023-04-03 12:01:08 +0200

    Improved implementations

--- src/main/scala/u05lab/ex1/List.scala
+++ src/main/scala/u05lab/ex1/List.scala
@@ -61,16 +61,20 @@ enum List[A]:
 
   /** EXERCISES */
   def zipRight: List[(A, Int)] =
-    foldRight(Nil())((a, b) => (a, this.length - b.length - 1) :: b)
+    foldRight(Nil())((e, b) => (e, b.head.map(_._2).getOrElse(this.length) - 1) :: b)
 
   def partition(pred: A => Boolean): (List[A], List[A]) =
     foldRight((List.Nil(), List.Nil()))({
       case (e, (p1, p2)) if pred(e) => (e :: p1, p2)
       case (e, (p1, p2)) => (p1, e :: p2)
     })
-    // foldRight((List.Nil(), List.Nil()))((e, ls) => if pred(e) then ls.copy(_1 = e :: ls._1) else ls.copy(_2 = e :: ls._2))
+    // foldRight((Nil(), Nil()))((e, ls) => if pred(e) then ls.copy(_1 = e :: ls._1) else ls.copy(_2 = e :: ls._2))
 
-  def span(pred: A => Boolean): (List[A], List[A]) = ???
+  def span(pred: A => Boolean): (List[A], List[A]) =
+    foldLeft((List.Nil(), List.Nil()))((a, e) => (e, a) match
+      case (e, (p1, p2)) if pred(e) && p2.isEmpty => (p1.append(List(e)), p2)
+      case (e, (p1, p2)) => (p1, p2.append(List(e)))
+    )
 
   /** @throws UnsupportedOperationException if the list is empty */
   def reduce(op: (A, A) => A): A = ???
--- src/test/scala/u05lab/ex1/ListTest.scala
+++ src/test/scala/u05lab/ex1/ListTest.scala
@@ -10,6 +10,8 @@ class ListTest {
   def testZipRight(): Unit =
     val l = List(1, 2, 3, 4, 5)
     assertEquals(List((1, 0), (2, 1), (3, 2), (4, 3), (5, 4)), l.zipRight)
+    assertEquals(List((1, 0), (2, 1), (3, 2), (4, 3)), List(1, 2, 3, 4).zipRight)
+    assertEquals(List((1, 0), (2, 1), (3, 2)), List(1, 2, 3).zipRight)
     assertEquals(List.Nil(), List.Nil().zipRight)
     assertEquals(List((1, 0)), List(1).zipRight)
 
@@ -18,6 +20,14 @@ class ListTest {
     val l = List(1, 2, 3, 4, 5)
     assertEquals((List(1, 3, 5), List(2, 4)), l.partition(_ % 2 == 1))
     assertEquals((List(1), List.Nil()), List(1).partition(_ % 2 == 1))
+
+  @Test
+  def testSpan(): Unit =
+    val l = List(1, 2, 3, 4, 5)
+    assertEquals((List(1, 2), List(3, 4, 5)), l.span(_ < 3))
+    assertEquals((List(1, 2, 3, 4, 5), List.Nil()), l.span(_ < 10))
+    assertEquals((List.Nil(), List(1, 2, 3, 4, 5)), l.span(_ < 0))
+    assertEquals((List.Nil(), List.Nil()), List.Nil[Int]().span(_ < 0))
 
 
 
